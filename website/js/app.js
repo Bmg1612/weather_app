ed928fe28dffc1d6795525b7403aa559028d09d4
Bruno Gurgel <bmg1612@gmail.com>
2020-11-14 12:55:11 -0300

    feat: Added dynamic list with results of the query ->
    - The results div will ony appear when the generate button is clicked

--- website/js/app.js
+++ website/js/app.js
@@ -1,10 +1,11 @@
 // Create a new date instance dynamically with JS
 const d = new Date();
 const newDate = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()}`;
+document.querySelector('#entryHolder').style.display = "none";
 // Personal API Key for OpenWeatherMap API
 const apiKey = '13f1473acdb89267f932793a8c56106a';
 const newZip = document.getElementById('zip').value;
-const baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${newZip},&units=metric&appid=${apiKey}`
+const baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${newZip},&appid=${apiKey}&units=metric`
 // Event listener to add function to existing HTML DOM element
 const newFeeling = document.getElementById('feeling').value;
 const button = document.querySelector('.button');
@@ -18,7 +19,8 @@ button.addEventListener('click', performAction = (e) => {
             temperature: data.main.temp,
             user_response: newFeeling
         });
-    });
+        updateUI();    
+    })
 });
 
 /* Function to GET Web API Data*/
@@ -56,3 +58,26 @@ const postData = async(url = '', data = {}) => {
 }
 
 /* Function to GET Project Data */
+/* Dynamically updating the UI */
+const updateUI = async () => {
+    let request = await fetch('/all');
+
+    let dateList = document.querySelector('#date_list');
+    let tempList = document.querySelector('#temperature_list');
+    let contentList = document.querySelector('#content_list');
+    let entryHolder = document.querySelector('#entryHolder');
+
+    entryHolder.style.display = "grid";
+
+
+    try {
+        let allData = await request.json();
+        console.log(allData);
+        dateList.innerHTML    = `<li class="query_item">Date: ${allData.date}</li>`;
+        tempList.innerHTML    = `<li class="query_item">Temperature: ${allData.temperature}</li>`;
+        contentList.innerHTML = `<li class="query_item">User response: ${allData.user_response}</li>`;
+        entryHolder.scrollIntoView({behavior: "smooth"});
+    } catch (error) {
+        console.log("error", error);
+    }
+}

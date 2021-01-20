9dc7103d1dd448910a3818012bdb9decbc6970ff
Bruno Gurgel <bmg1612@gmail.com>
2021-01-20 13:12:04 -0300

    chore: Updated code to match portfolio

--- website/js/app.js
+++ website/js/app.js
@@ -1,94 +1,118 @@
 /*
-* Setting the display of the results div to none so that
-* it only appears when the generate button is clicked
-*/
-document.querySelector('#entryHolder').style.display = "none";
+ * Setting the display of the results div to none so that
+ * it only appears when the generate button is clicked
+ */
+document.querySelector("#entryHolder").style.display = "none";
 
 // HTML DOM target
-const button = document.querySelector('.button');
+const button = document.querySelector(".button");
 
 // Event listener to add function to existing HTML DOM element
-button.addEventListener('click', performAction = () => {
-    // Create a new date instance dynamically with JS
-    const d = new Date();
-    const newDate = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()}`;
+button.addEventListener("click", () => {
+  let apiKey = "";
 
-    const newZip = document.getElementById('zip').value;
-    const newFeeling = document.getElementById('feeling').value;
-    // Personal API Key for OpenWeatherMap API
-    const apiKey = 'xxxxxxxxxxxxxxxxxxxx';
-    const baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${newZip},&appid=${apiKey}&units=metric`;
-    if (newFeeling != "" && newZip != "") {
-    displayWeather(baseUrl)
-    .then ((data) => {
-        postData('/data', {
+  // Create a new date instance dynamically with JS
+  const d = new Date();
+  const newDate = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()}`;
+
+  const newZip = document.getElementById("zip").value;
+  const newFeeling = document.getElementById("feeling").value;
+
+  document.querySelector("#entryHolder").style.display = "none";
+  document.querySelector(".loader").style.display = "inline-block";
+  document.querySelector(".loader").scrollIntoView({ behavior: "smooth" });
+
+  getApiKey().then(() => {
+    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${newZip},&appid=${apiKey}&units=metric`;
+    if (newFeeling !== "" && newZip !== "") {
+      displayWeather(baseUrl)
+        .then((data) => {
+          return postData("/data", {
             date: newDate,
             temperature: data.main.temp,
-            user_response: newFeeling
-        });
-        updateUI();       
-    })
-  } else {
+            user_response: newFeeling,
+          });
+        })
+        .then(() => updateUI());
+    } else {
       alert("Please enter Zip code and your feelings");
-  }
-});
-
-/* Function to GET Web API Data*/
-const displayWeather = async (url) => {
-    let request = await fetch(url);
-    if (request.ok) {
-    try {
-        const data = await request.json();
-        console.log(data);
-        return data;
-    } catch(error) {
-        Alert("There was an error:", error.message);
     }
- } else if (request.status === 404) {
-     alert("City not found. Are you sure the Zip is from USA?");
- } else {
-     alert("There was an error: " + request.statusText)
- }
-}
+  });
 
-/* Function to POST data */
-const postData = async(url = '', data = {}) => {
-    let response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+  async function getApiKey() {
+    const req = await fetch("/api");
+    try {
+      const data = await req.json();
+      console.log(data);
+      apiKey = data.key;
+      console.log(apiKey);
+      return apiKey;
+    } catch (error) {
+      alert("There was an error:", error.message);
+      return false;
+    }
+  }
+
+  /* Function to GET Web API Data */
+  async function displayWeather(url) {
+    const request = await fetch(url);
+    if (request.ok) {
+      try {
+        const data = await request.json();
+        return data;
+      } catch (error) {
+        return alert("There was an error:", error.message);
+      }
+    } else if (request.status === 404) {
+      return alert("City not found. Are you sure the Zip is from USA?");
+    } else {
+      return alert(`There was an error: ${request.statusText}`);
+    }
+  }
+
+  /* Function to POST data */
+  async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     });
 
     try {
-        let newData = await response.json();
-        // console.log(newData);
-        return newData
-    } catch(error) {
-        console.log("There was an error:", error);
+      const newData = await response.json();
+      return newData;
+    } catch (error) {
+      alert("There was an error:", error);
+      return false;
     }
-}
+  }
 
-/* Dynamically updating the UI */
-const updateUI = async () => {
-    let request = await fetch('/data');
+  /* Dynamically updating the UI */
+  async function updateUI() {
+    const request = await fetch("/data");
 
-    let dateList = document.querySelector('#date_list');
-    let tempList = document.querySelector('#temperature_list');
-    let contentList = document.querySelector('#content_list');
-    let entryHolder = document.querySelector('#entryHolder');
-    //Showing the results div when the button is clicked
-    entryHolder.style.display = "grid";
+    const dateList = document.querySelector("#date_list");
+    const tempList = document.querySelector("#temperature_list");
+    const contentList = document.querySelector("#content_list");
+    const entryHolder = document.querySelector("#entryHolder");
 
     try {
-        let allData = await request.json();
-        dateList.innerHTML    = `<li class="query_item">Date: ${allData.date}</li>`;
-        tempList.innerHTML    = `<li class="query_item">Temperature: ${allData.temperature.toFixed(0)}ºC</li>`;
-        contentList.innerHTML = `<li class="query_item">Feeling: ${allData.user_response}</li>`;
-        entryHolder.scrollIntoView({behavior: "smooth"});
+      const allData = await request.json();
+      dateList.innerHTML = `<li class="query_item">Date: ${allData.date}</li>`;
+      tempList.innerHTML = `<li class="query_item">Temperature: ${allData.temperature.toFixed(
+        0
+      )}ºC</li>`;
+      contentList.innerHTML = `<li class="query_item">Feeling: ${allData.user_response}</li>`;
+      document.querySelector(".loader").style.display = "";
+      document.querySelector("#entryHolder").style.display = "";
+      //Showing the results div when the button is clicked
+      entryHolder.style.display = "grid";
+      entryHolder.scrollIntoView({ behavior: "smooth" });
     } catch (error) {
-        console.log("There was an error:", error);
+      console.log("There was an error:", error);
     }
-}
+  }
+});
